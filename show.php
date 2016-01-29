5f6c338eb6e525f11fae94a6c43e1a666a89d4be
Vishnu A Venu <vishucool.venu@gmail.com>
2016-01-29 17:07:11 +0530

    cosmetic changes

--- show.php
+++ show.php
@@ -132,53 +132,17 @@ else{e.value="no";location.reload();}
 								<span class="icon-bar"></span>
 							</button>
 							<a class="navbar-brand" href="#">
-								<img src="img/header-logo.png" alt="">
+								<img src="img/logo.png" alt="">
 							</a>
 						</div>
 
 						<!-- Collect the nav links, forms, and other content for toggling -->
 						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 							<ul class="nav navbar-nav navbar-right">
-								<li class="dropdown">
-									<a href="#" class="dropdown-toggle" data-toggle="dropdown">Home <span class="caret"></span></a>
-									<ul class="dropdown-menu" role="menu">
-										<li><a href="index.html">Home-1</a></li>
-										<li><a href="index-landingpage.html">Home landing</a></li>
-									</ul>
-								</li>
-								<li class="dropdown">
-									<a href="#" class="dropdown-toggle act" data-toggle="dropdown">Menu Card <span class="caret"></span></a>
-									<ul class="dropdown-menu" role="menu">
-										<li><a href="menu(view-1).html">Menu( view-1 )</a></li>
-										<li><a href="menu(view-2).html">Menu( view-2 )</a></li>
-										<li><a href="menu-without-side-panel.html">Menu( grid-view )</a></li>
-										<li><a href="single-menu.html">Single Menu</a></li>
-										<li><a href="menu(2grid-list).html">Menu( 2grid-list-view )</a></li>
-										<li><a href="menu-with-right-checkout.html">Menu( with-checkout )</a></li>
-										<li><a href="dish.html">Dish</a></li>
-										<li><a href="dish-with-no-sidebar.html">Dish ( No Sidebar )</a></li>
-										<li><a href="shortcodes.html">Shortcodes</a></li>
-									</ul>
-								</li>
-								<li><a href="chef.html">Chef</a>
-								</li>
-								<li><a href="our-team.html">Team</a>
+								<li>
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown">Home</a>
 								</li>
 								
-								<li class="dropdown">
-									<a href="#" class="dropdown-toggle" data-toggle="dropdown">Blog <span class="caret"></span></a>
-									<ul class="dropdown-menu" role="menu">
-										<li><a href="blog-list.html">Blog List</a></li>
-										<li><a href="blog-post.html">Blog Post</a></li> 
-									</ul>
-								</li>
-								<li class="dropdown">
-									<a href="#" class="dropdown-toggle" data-toggle="dropdown">News & Events <span class="caret"></span></a>
-									<ul class="dropdown-menu" role="menu">
-										<li><a href="news-events.html">News & Events list</a></li>
-										<li><a href="event-page.html">News & Events post</a></li> 
-									</ul>
-								</li>
 								<li><a href="contact-us.html">Contact us</a>
 								</li>
 							</ul>
@@ -543,10 +507,7 @@ else{e.value="no";location.reload();}
 														<!--end all-menu-details-->
 
 
-														<div class="all-menu-details">
-															<h5>Main Course</h5>
-
-														</div> <!-- end .all menu details -->
+														
 
 													</div> <!-- end .tab-pane -->
 
@@ -570,7 +531,9 @@ else{e.value="no";location.reload();}
 														<li>
 															<!-- list for total price-->
 															<p>Total</p>
-															<p class="price-total">$ 35.95</p>
+															<p id="price-total" class="price-total">
+																
+															</p>
 														</li>
 													</ul>
 
@@ -1072,16 +1035,10 @@ else{e.value="no";location.reload();}
 						itemli.appendChild(customization);
 						document.getElementById('cartlist').appendChild(itemli);
 
-
-
-
-
-
-
-
-
-
-					}}}
+					}
+				}
+				console.log(final_order_list);
+			}
 
 					function removeFromCart(item)
 					{
